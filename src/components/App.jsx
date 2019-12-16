2026652426a220371f9797ed132a19392280165e
imhta <imhtapm@gmail.com>
2019-12-16 20:01:45 +0530

    :heavy_plus_sign: add app component in components import in index js

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Display from './Display';
+import './App.scss';
+import ButtonPanel from './ButtonPanel';
+
+function App() {
+  return (
+    <div className="App">
+        <Display result='0'/>
+        <ButtonPanel/>
+    </div>
+  );
+}
+
+export default App;
