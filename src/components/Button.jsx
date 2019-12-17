a3e3131254813ef442669f4a474a1e830bd2cdcf
imhta <imhtapm@gmail.com>
2019-12-17 15:22:10 +0530

    :art: style the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ name }) {
-  return <button>{name}</button>;
+function Button({ name, wide, color }) {
+  const style = {
+    width: wide ? '50%' : '25%',
+    backgroundColor: color,
+    border: '1px solid #d1d1d1',
+    fontSize: '2rem',
+    cursor: 'pointer',
+  };
+  return <button style={style}>{name}</button>;
 }
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  wide: PropTypes.bool,
+  color: PropTypes.string,
+};
+
+Button.defaultProps = {
+  wide: false,
+  color: 'orange',
 };
 export default Button;
