392e8d37458d423bb380db76baffadd92f729185
imhta <imhtapm@gmail.com>
2019-12-17 15:22:35 +0530

    :art: style the button panel component

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -3,35 +3,36 @@ import './ButtonPanel.scss';
 import Button from './Button.jsx';
 
 function ButtonPanel() {
+  const grey = '#e0e0e0';
   return (
-    <section>
+    <section className="ButtonPanel">
       <div className="row">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
+        <Button name="AC" color={grey} />
+        <Button name="+/-" color={grey} />
+        <Button name="%" color={grey} />
         <Button name="÷" />
       </div>
       <div className="row">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
+        <Button name="7" color={grey} />
+        <Button name="8" color={grey} />
+        <Button name="9" color={grey} />
         <Button name="X" />
       </div>
       <div className="row">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
+        <Button name="4" color={grey} />
+        <Button name="5" color={grey} />
+        <Button name="6" color={grey} />
         <Button name="-" />
       </div>
       <div className="row">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
+        <Button name="1" color={grey} />
+        <Button name="2" color={grey} />
+        <Button name="3" color={grey} />
         <Button name="+" />
       </div>
       <div className="row">
-        <Button name="0" />
-        <Button name="." />
+        <Button name="0" color={grey} wide />
+        <Button name="." color={grey} />
         <Button name="=" />
       </div>
     </section>
