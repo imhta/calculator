1a4d5773d7879beea03e1130b83e262d234013e7
imhta <imhtapm@gmail.com>
2019-12-18 22:03:56 +0530

    :up: handle click from btn and change default color

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,42 +1,47 @@
 import React from 'react';
 import './ButtonPanel.scss';
+import PropTypes from 'prop-types';
 import Button from './Button.jsx';
 
-function ButtonPanel() {
-  const grey = '#e0e0e0';
+function ButtonPanel({ clickHandler }) {
+  const handleClick = (buttonName) => clickHandler(buttonName);
   return (
     <section className="ButtonPanel">
       <div className="row">
-        <Button name="AC" color={grey} />
-        <Button name="+/-" color={grey} />
-        <Button name="%" color={grey} />
-        <Button name="÷" />
+        <Button name="AC" clickHandler={handleClick} />
+        <Button name="+/-" clickHandler={handleClick} />
+        <Button name="%" clickHandler={handleClick} />
+        <Button name="÷" color="orange" clickHandler={handleClick} />
       </div>
       <div className="row">
-        <Button name="7" color={grey} />
-        <Button name="8" color={grey} />
-        <Button name="9" color={grey} />
-        <Button name="x" />
+        <Button name="7" clickHandler={handleClick} />
+        <Button name="8" clickHandler={handleClick} />
+        <Button name="9" clickHandler={handleClick} />
+        <Button name="x" color="orange" clickHandler={handleClick} />
       </div>
       <div className="row">
-        <Button name="4" color={grey} />
-        <Button name="5" color={grey} />
-        <Button name="6" color={grey} />
-        <Button name="-" />
+        <Button name="4" clickHandler={handleClick} />
+        <Button name="5" clickHandler={handleClick} />
+        <Button name="6" clickHandler={handleClick} />
+        <Button name="-" color="orange" clickHandler={handleClick} />
       </div>
       <div className="row">
-        <Button name="1" color={grey} />
-        <Button name="2" color={grey} />
-        <Button name="3" color={grey} />
-        <Button name="+" />
+        <Button name="1" clickHandler={handleClick} />
+        <Button name="2" clickHandler={handleClick} />
+        <Button name="3" clickHandler={handleClick} />
+        <Button name="+" color="orange" clickHandler={handleClick} />
       </div>
       <div className="row">
-        <Button name="0" color={grey} wide />
-        <Button name="." color={grey} />
-        <Button name="=" />
+        <Button name="0" wide clickHandler={handleClick} />
+        <Button name="." clickHandler={handleClick} />
+        <Button name="=" color="orange" clickHandler={handleClick} />
       </div>
     </section>
   );
 }
+
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default ButtonPanel;
