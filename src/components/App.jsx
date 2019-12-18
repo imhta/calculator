e0b1ae4d9d4547f646b1120705d6bbc29e60b8a3
imhta <imhtapm@gmail.com>
2019-12-18 22:03:23 +0530

    :up: intregate handle click and add more state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display.jsx';
 import './App.scss';
 import ButtonPanel from './ButtonPanel.jsx';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate.js';
 
 function App() {
+  const [state, setState] = useState({
+    queue: [], total: '', next: '', operation: '', target: 'total',
+  });
+  const handleClick = (btnName) => {
+    setState(calculate(state, btnName));
+  };
   return (
     <div id="App">
-      <Display result="0" />
-      <ButtonPanel />
+      <Display result={state.queue.join('') || '0'} />
+      <ButtonPanel clickHandler={handleClick} />
     </div>
   );
 }
