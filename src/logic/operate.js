ebd7ce6dd5fa5cbb9d96851d500bbbcbc0a2fd0f
imhta <imhtapm@gmail.com>
2019-12-18 22:06:25 +0530

    :up: :hammer: refactor case with new symbol case

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,18 +1,16 @@
 import Big from 'big.js';
 
 const big100 = Big(100);
-// const bigZero = Big(0);
-// const bigOne = Big(1);
 
 const operate = (operation, numOne, numTwo) => {
   numOne = Big(numOne);
   numTwo = Big(numTwo);
   const operateWith = {
-    '+': (numOne, numTwo) => numOne.plus(numTwo).toString(),
-    '-': (numOne, numTwo) => numOne.minus(numTwo).toString(),
-    '*': (numOne, numTwo) => numOne.times(numTwo).toString(),
-    '/': (numOne, numTwo) => numOne.div(numTwo).toString(),
-    '%': (numOne) => numOne.div(big100),
+    '+': (numOne, numTwo) => numOne.plus(numTwo || 0).toString(),
+    '-': (numOne, numTwo) => numOne.minus(numTwo || 0).toString(),
+    x: (numOne, numTwo) => numOne.times(numTwo || 1).toString(),
+    '÷': (numOne, numTwo) => numOne.div(numTwo || 1).toString(),
+    '%': (numOne) => numOne.div(big100).toString(),
   };
   return operateWith[operation](numOne, numTwo);
 };
