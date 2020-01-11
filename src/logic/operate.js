0affd7d8c4af97d1baed856dd68f0329c6588010
imhta <imhtapm@gmail.com>
2020-01-11 15:07:09 +0530

    :fire: remove comments and :hammer:  refactor % to mod operation

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,8 +1,5 @@
 import Big from 'big.js';
 
-const big100 = Big(100);
-// const bigZero = Big(0);
-// const bigOne = Big(1);
 
 const operate = (operation, numOne, numTwo) => {
   numOne = Big(numOne);
@@ -12,7 +9,7 @@ const operate = (operation, numOne, numTwo) => {
     '-': (numOne, numTwo) => numOne.minus(numTwo).toString(),
     '*': (numOne, numTwo) => numOne.times(numTwo).toString(),
     '/': (numOne, numTwo) => numOne.div(numTwo).toString(),
-    '%': (numOne) => numOne.div(big100),
+    '%': (numOne, numTwo) => numOne.mod(numTwo).toString(),
   };
   return operateWith[operation](numOne, numTwo);
 };
